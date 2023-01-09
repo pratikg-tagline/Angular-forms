5270735f8ff6098e8eae05824f36d28c605f9b40
pratikg-tagline <pratikg.tagline@gmail.com>
2023-01-09 10:20:15 +0530

    Implement extra template driven in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,13 +8,42 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'angular-forms';
-  form:any
-  
+  form:any;
+  data:any;
+
+  array:any=[
+
+    { id:1,
+      firstname:'pratik',
+    lastname:'ghoghari',
+     email:'pratik@gmail.com',
+     country:'india',
+     subject:'xyz'
+    },{
+      id:2,
+      firstname:'raj',
+    lastname:'variya',
+     email:'raj@gmail.com',
+     country:'india',
+     subject:'abc'
+    }
+]
+ @ViewChild ('myForm') myForm!:NgForm
+
   onsubmit(form:NgForm){
     console.log(form)
   }
+  datashow(myForm:any){
+    this.array.push(this.myForm.value);
+    console.log('data :>> ', this.array);
+  }
+ 
   defultcountry='india';
   firstname:string | undefined
   lastname:string | undefined
   email:any
+
+  BookDelete(data: number){
+    this.array.splice(data,1);
+  }
 }
