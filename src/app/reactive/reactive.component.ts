ae77e531b47b64b5dfee1194feb7ad255dc4c975
pratikg-tagline <pratikg.tagline@gmail.com>
2023-01-18 16:31:19 +0530

    Implement reactive address in project

--- src/app/reactive/reactive.component.ts
+++ src/app/reactive/reactive.component.ts
@@ -9,10 +9,11 @@ interface Array {
   email: string;
   gender: string;
   phone: string;
-  country:string;
+  address:{country:string;
   state:string;
-  city:string;
+  city:string;}
 }
+
 @Component({
   selector: 'app-reactive',
   templateUrl: './reactive.component.html',
@@ -31,7 +32,11 @@ export class ReactiveComponent implements OnInit {
   delete: string = 'Delete';
   country:string='Country'
   state:string='State';
-  city:string='City'
+  city:string='City';
+
+  states:any
+  citys:any
+ 
 
 
   array: Array[] = [
@@ -42,9 +47,9 @@ export class ReactiveComponent implements OnInit {
       email: 'pratik@gmail.com',
       gender: 'male',
       phone: '990-40-677-22',
-      country:'india',
+      address:{country:'india',
       state:'gujrat',
-      city:'surat'
+      city:'surat'}
     },
     {
       id:'2',
@@ -53,14 +58,37 @@ export class ReactiveComponent implements OnInit {
       email: 'raj@gmail.com',
       gender: 'india',
       phone: '990-80-522-22',
-      country:'india',
+      address:{country:'india',
       state:'gujrat',
-      city:'surat'
+      city:'surat'}
     },
   ];
 
 
+  countryList: any = [
+    { id: 1, name: 'India' },
+    {id:2,name:'japan'}
+  ];
 
+  stateList: any = [
+    { id: 1, name: 'Gujarat', country: 1 },
+    { id: 2, name: 'Maharastra', country: 1 },
+    {id:3,name:'Hokkaido',country:2},
+    {id:4,name:'Tohoku',country:2},
+  ];
+
+  cityList: any = [
+    { id: 1, name: 'surat', state: 1 },
+    { id: 2, name: 'Ahmedabad', state: 1 },
+    { id: 3, name: 'Mumbai', state: 2 },
+    { id: 4, name: 'nasik', state: 2 },
+    {id:5,name:'Tokyp',state:3},
+    {id:6,name:'Kyato',state:3},
+    {id:7,name:'sapparo',state:3},
+    {id:8,name:'kobe',state:4},
+    {id:9,name:'nagoya',state:4},
+    {id:10,name:'Osaka',state:4},
+  ];
 
   myForm: any;
   submit: string = 'Submit';
@@ -73,6 +101,11 @@ export class ReactiveComponent implements OnInit {
       email: this.formBuilder.control('', Validators.required),
       gender: this.formBuilder.control('', Validators.required),
       phone: this.formBuilder.control('', Validators.required),
+      address: this.formBuilder.group({
+        country: this.formBuilder.control('', Validators.required),
+        state: this.formBuilder.control('', Validators.required),
+        city:this.formBuilder.control('', Validators.required),
+      }),
     });
   }
 
@@ -90,8 +123,27 @@ export class ReactiveComponent implements OnInit {
     this.signupForm.patchValue(data);
     console.log('data :>> ', data);
   }
-
+  changeCountry(c:any){
+    this.states = this.stateList.filter(
+      (con: any) => con.country == c.target.value
+    );
+  }
+  changeState(d:any){
+    this.citys = this.cityList.filter(
+      (con: any) => con.state == d.target.value
+    );
+  }
   datashow(signupForm: any) {
+     const add = this.signupForm.value.address;
+    const currentCountry = this.countryList.find(
+      (country: any) => country.id == add.country
+    ).name;
+    const currentState = this.stateList.find(
+      (state: any) => state.id == add.state
+    ).name;
+    const currentCity = this.cityList.find(
+      (cit: any) => cit.id == add.city
+    ).name;
     if (this.signupForm.invalid) {
       return;
     } else {
@@ -103,11 +155,21 @@ export class ReactiveComponent implements OnInit {
         );
         this.array[index] = {
           ...this.signupForm.value,
+          address: {
+            country: currentCountry,
+            state: currentState,
+            city: currentCity,
+          },
         };
       } else {
         const data = {
           id: this.array.length + 1,
           ...this.signupForm.value,
+          address: {
+            country: currentCountry,
+            state: currentState,
+            city: currentCity,
+          },
         };
       
 
@@ -120,6 +182,8 @@ export class ReactiveComponent implements OnInit {
     console.log('this.reactiveForm.value :>> ', this.signupForm);
   }
 }
+
+
   // postData(signupForm:any):void{
   //   this.data=this.signupForm.value
   //     console.log(this.data)
